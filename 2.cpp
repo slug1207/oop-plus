18722b69543fdab9b2fe74614dec93eb6ac8ad61
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 21:57:13 +0800

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main(){
 	int input;
-	while(cin>>input)//讀入整數 
+	while(cin>>input)
 	{
-		cout<<fixed<<setprecision(1)<<(input*1.6L*10/10)<<endl;
-		/*輸出<<指定浮點數表示<<小數點後幾位(預設為6)
-		<<整數轉換小數(乘L)並確保小數點後僅一位<<換行*/ 
+		float a = input*1.6;
+		cout<<fixed<<setprecision(1)<<a<<endl;
 	}
 	return 0;
 }
