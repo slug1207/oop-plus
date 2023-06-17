7ddaf2a4ba992f908c370c46cdcb7dc7a88d44c4
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 22:03:59 +0800

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -8,10 +8,10 @@ int main()
 	cin>>input;
 	switch(input/3)//取商數為區分點 
 	{
-		case 1:cout<<"Spring"<<endl;break;//商數為1，(3,4,5) 
-		case 2:cout<<"Summer"<<endl;break;//商數為2，(6,7,8)
-		case 3:cout<<"Autumn"<<endl;break;//商數為3，(9,10,11)
-		default :cout<<"Winter"<<endl;//其餘(12(商數為4),1,2)皆為冬季 
+		case 1:cout<<"Spring"<<endl;break; 
+		case 2:cout<<"Summer"<<endl;break;
+		case 3:cout<<"Autumn"<<endl;break;
+		default :cout<<"Winter"<<endl; 
 	} 
 	return 0;
 } 
