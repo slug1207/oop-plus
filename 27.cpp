4f36685a42ce5db0e26cb4909997baa2ba4c434d
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 22:35:53 +0800

    Update 27.cpp

--- 27.cpp
+++ 27.cpp
@@ -2,27 +2,34 @@
 using namespace std;
 int main()
 {
-	int an=0,ana[4],ain[4];
+	int an=0,ans[4],ain[4];
 	cin>>an;
-	for(int i=3;i>=0;i--)//逆向讀入陣列 
+	for(int i=3;i>=0;i--)
 	{
-		ana[i]=0;ain[i]=0;
-		ana[i]=an%10;an/=10;
+		ans[i]=0;
+		ans[i]=an%10;an/=10;
 	}
-	while(true)//保持無限迴圈狀態 
+	while(true) 
 	{
 		int in=0,a=0,b=0;
 		cin>>in;
-		an=0;//解除無限迴圈用 
-		for(int i=3;i>=0;i--)//確保位置與答案相同 
+		int flag=0; 
+		for(int i=3;i>=0;i--)//逆向讀入陣列 
 		{
-			ain[i]=in%10;in/=10;//逆向讀入陣列 
-			if(ain[i]==0)an++;//判定0的個數 
-			if(ain[i]==ana[i]){a++;continue;}//確認同位置且同答案 
-			else {for(int j=0;j<4;j++){if(ain[i]==ana[j]&&j!=i){b++;break;}}}//尋找其餘3個的位置是否有同樣答案 
+			ain[i]=in%10;
+			in/=10; 
+			if(ain[i]==0)flag++;//判定0的個數 
+			if(ain[i]==ans[i]){a++;continue;} 
+			else {
+				for(int j=0;j<4;j++){
+					if(ain[i]==ans[j]&&j!=i){
+						b++;
+						break;}
+				}
+			} 
 		}
-		if(an==4)break;//判定是否4個0 
-		else cout<<a<<"A"<<b<<"B"<<endl;//輸出答案 
+		if(flag==4)break; 
+		else cout<<a<<"A"<<b<<"B"<<endl; 
 	}
 	return 0;
 } 
