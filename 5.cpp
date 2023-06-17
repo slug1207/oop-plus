941afb32af73694c6531f11210d35b971823516a
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 22:03:16 +0800

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 int main()
 {
-	int input=0,Bin[8];
+	int input=0,A[8];
 	
 	cin>>input;
 	if(input<0){
-        input+=256;//負數則推移至正整數 
+        input+=256;
     } 
-	for(int i=7;i>=0;i--)//逆向遞減輸入 
+	for(int i=7;i>=0;i--) 
     {
-    	Bin[i]=input%2;//將餘數結果存入 
-    	input/=2;//每算出一位數則留商數繼續往下算 
+    	A[i]=input%2; 
+    	input/=2; 
     }
-    for(int i=0;i<8;i++)//順向依序輸出 
+    for(int i=0;i<8;i++)
     {
     	if(i==7){
-            cout<<Bin[i]<<endl;//若為最後一位則換行 
+            cout<<A[i]<<endl;
         }
     	else{
-            cout<<Bin[i];//否則緊接在後輸出 
+            cout<<A[i]; 
         }  
     }	
 	return 0;
