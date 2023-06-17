7a7b28eafbf4428c9b4fbe8d199034fdbcdd00b1
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 22:09:26 +0800

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -2,21 +2,20 @@
 #include<cmath>
 using namespace std;
 
-int m (int input);
+int A (int input);
 int main()
 {
 	int input=0;
-	
-	cin>>input;//輸入整數 
-	cout<<m(input)<<endl;//輸出函式的值 
+	cin>>input; 
+	cout<<A(input)<<endl;
 	return 0;
 }
-int m (int input)
+int A (int input)
 {
-	if(input==0||input==1)//當輸入值為1或0 
+	if(input==0||input==1) 
 	{
 		return input+1;
 	}
-	else return m(input-1)+m(ceil(input/2));
-	//因題目寫明，輸入值為大於1的整數，所以不需擔心負值 
+	else return A(input-1)+A(ceil(input/2));
+	 
 } 
