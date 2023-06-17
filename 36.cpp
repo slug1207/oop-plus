7dc6f090a0e589c22fb8947f301ae29a96709a4b
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 22:42:47 +0800

    Update 36.cpp

--- 36.cpp
+++ 36.cpp
@@ -5,10 +5,14 @@ int main()
 	int in=0;
 	while(cin>>in)
 	{
-		if(in%400==0)cout<<"Bissextile Year"<<endl;
+		if(in%400==0){
+			cout<<"Bissextile Year"<<endl;
+		}
 		else
 		{
-			if(in%100==0)cout<<"Common Year"<<endl;
+			if(in%100==0){
+				cout<<"Common Year"<<endl;
+			}
 			else
 			{
 				in%4==0?cout<<"Bissextile Year"<<endl:cout<<"Common Year"<<endl;
