3ef8ed4b390218646e9bfb75b1349e2358a63f17
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 22:04:42 +0800

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -6,14 +6,14 @@ int main()
 	int input=0;
 	
 	cin>>input;
-	while(input--)//依據輸入的行數遞減計算次數 
+	while(input--)
 	{
 		int a=0,b=0,c=0,d=0;
-		char w;//運算符號 
+		char w; 
 		
 		cin>>w>>a>>b>>c>>d;
-		switch(w)//依據運算符號來決定計算方式 
-		{//依據運算符號輸出計算公式的答案 
+		switch(w) 
+		{ 
 			case '+':cout<<a+c<<" "<<b+d<<endl;break;
 			case '-':cout<<a-c<<" "<<b-d<<endl;break;
 			case '*':cout<<a*c-b*d<<" "<<b*c+a*d<<endl;break;
