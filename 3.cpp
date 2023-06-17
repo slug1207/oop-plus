9cf83e074717274bc49498af4e0668642472221d
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 21:58:49 +0800

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main()
 {
-	int inputx=0,inputy=0;
-	while(cin>>inputx>>inputy)//依序讀入X，Y值 
+	int x=0,y=0;
+	while(cin>>x>>y)
 	{
-        int a = inputx, b = inputy; 
+        int a = x, b = y; 
 		if(a*a+b*b <= 10000){
             cout<<"inside"<<endl;
         } 
-		//若X平方+Y平方小於半徑(100)的平方，輸出"inside" 
 		else{
             cout<<"outside"<<endl;
         } 
 	}
 	return 0;
 } 
+	
