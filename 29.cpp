bb60c4d923577607d0e163c060aa25daa6632e5a
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-06-17 22:37:42 +0800

    Update 29.cpp

--- 29.cpp
+++ 29.cpp
@@ -4,14 +4,20 @@ int main()
 {
 	string in;
 	getline(cin,in);
-	if(((int)in[1]-'0')<1||((int)in[1]-'0')>2){cout<<"WRONG!!!"<<endl;return 0;}//性別檢測 
+	if(((int)in[1]-'0')<1||((int)in[1]-'0')>2){
+		cout<<"WRONG!!!"<<endl;return 0;
+	}//性別檢測 
 	for(int i=1;i<9;i++)//數字區域是否為數字 
 	{
-		if(in[i]<'0'||in[i]>'9'){cout<<"WRONG!!!"<<endl;return 0;}
+		if(in[i]<'0'||in[i]>'9'){
+			cout<<"WRONG!!!"<<endl;return 0;
+		}
 	}
 	int p=0,loc=0;
 	loc=(int)in[0]; 
-	if(loc<'A'||loc>'Z'){cout<<"WRONG!!!"<<endl;return 0;}//區域碼轉換 
+	if(loc<'A'||loc>'Z'){
+		cout<<"WRONG!!!"<<endl;return 0;
+	}//區域碼轉換 
 	else if(loc<=72){loc-=55;}//A~H
 	else if(loc==73){loc=34;}//I
 	else if(loc>73&&loc<=78){loc-=56;}//J~N 
@@ -22,5 +28,6 @@ int main()
 	p=(loc/10)+9*(loc%10);//取十位數再加上個位數*9 
 	for(int i=1;i<9;i++){p+=(9-i)*((int)in[i]-'0');}//加上之後的公式總和 
 	if(10-(p%10)!=((int)in[9]-'0')){cout<<"WRONG!!!"<<endl;return 0;}//檢查碼判定 
-	else {cout<<"CORRECT!!!"<<endl;return 0;}	
+	else cout<<"CORRECT!!!"<<endl;
+	return 0;	
 }
